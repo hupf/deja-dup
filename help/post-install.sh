46b0b744f0f53f57caf92b87e37d43a1d2adc994
Michael Terry <mike@mterry.name>
2020-06-01 19:08:14 -0400

    Update help documentation
    Fix credits page to be more accurate and drop translator credits
    in the English version.
    
    Drop some pages that make less sense with redesign and update text
    that wasn't accurate anymore.

---
+++ help/post-install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# -*- Mode: sh; indent-tabs-mode: nil; tab-width: 2; coding: utf-8 -*-
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: Michael Terry
+
+datadir="$1"
+
+# Drop the translation section from the non-translated help
+sed -i 's/.*translation-credits.*//' "$datadir/help/C/deja-dup/credits.page"
